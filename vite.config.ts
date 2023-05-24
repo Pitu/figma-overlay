6a79c630e9ce1dea7442e110806eb52e3461c825
Kana <heyitspitu@gmail.com>
2023-05-24 16:02:05 -0300

    chore: proper types

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
+import { resolve } from 'node:path';
+import process from 'node:process';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,11 +13,14 @@ export default defineConfig({
 					isCustomElement: tag => tag.includes('figma-overlay')
 				}
 			}
+		}),
+		dts({
+			insertTypesEntry: true
 		})
 	],
 	build: {
 		lib: {
-			entry: './src/main.ce.ts',
+			entry: resolve(__dirname, 'src/main.ce.ts'),
 			name: 'figma-overlay',
 			// the proper extensions will be added
 			fileName: 'figma-overlay'
